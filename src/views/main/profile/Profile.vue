b98b391f83c96527188bfb51726762a094c09043
LinXyyy <1508300855@qq.com>
2021-07-15 10:04:30 +0800

    v1.3

--- src/views/main/profile/Profile.vue
+++ src/views/main/profile/Profile.vue
@@ -10,7 +10,43 @@
       喝水选手
     </div>
     <div>
-      ad
+      <v-row style="margin: 20px; padding: 0;">
+        <v-spacer/>
+        <v-col>
+          <v-menu open-on-hover top offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon v-bind="attrs" v-on="on">
+                <v-icon>
+                  mdi-qqchat
+                </v-icon>
+              </v-btn>
+            </template>
+
+            <v-img :src="qq" height="150px" width="150px"/>
+          </v-menu>
+        </v-col>
+        <v-col>
+          <v-menu open-on-hover top offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon v-bind="attrs" v-on="on">
+                <v-icon>
+                  mdi-wechat
+                </v-icon>
+              </v-btn>
+            </template>
+
+            <v-img :src="vx" height="150px" width="150px"/>
+          </v-menu>
+        </v-col>
+        <v-col>
+          <v-btn icon @click="toGithub">
+            <v-icon>
+              mdi-github
+            </v-icon>
+          </v-btn>
+        </v-col>
+        <v-spacer/>
+      </v-row>
     </div>
   </v-card>
 </template>
@@ -19,8 +55,17 @@
   export default {
     name: "Profile",
     data:() => ({
-      avatar: require("@/assets/avatar.jpg")
+      avatar: require("@/assets/avatar.jpg"),
+      qq: require("@/assets/qq.png"),
+      vx: require("@/assets/vx.png")
     }),
+    methods: {
+      toGithub() {
+        let a = document.createElement('a')
+        a.href = 'https://github.com/LinXyyy'
+        a.click()
+      }
+    }
   }
 </script>
 
